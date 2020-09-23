36f6bdc2b8f71559697af9007a3fa2c44847b4d2
katniss07 <natcarrato@gmail.com>
2020-09-23 04:08:08 -0300

    imc-calculadora

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,97 +1,33 @@
-// capturar evento de submit do formulário
+const calcular = document.getElementById('calcular');
 
-const form = document.querySelector('#formulario');
+const imc = () => {
+  const nome = document.getElementById('nome').value;
+  const altura = document.getElementById('altura').value;
+  const peso = document.getElementById('peso').value;
+  const resultado = document.getElementById('resultado');
 
-form.addEventListener('submit', function (e) {
-  // addEventListener adicina um
-  // evento, que possui 2 parâmetros: um callback e um evento.
-  // callback é uma função dentro de outra função
-  e.preventDefault(); // adiciona no evento
-  const inputPeso = e.target.querySelector('#peso');
-  const inputAltura = e.target.querySelector('#altura');
+  if (nome !== '' && altura !== '' && peso !== '') {
+    const valorIMC = (peso / altura ** 2).toFixed(2);
+    let classificacao = '';
 
-  const peso = Number(inputPeso.value);
-  const altura = Number(inputAltura.value);
+    if (valorIMC < 18.5) {
+      classificacao = 'abaixo do peso.';
+    } else if (valorIMC < 25) {
+      classificacao = 'com peso ideal. Parabéns!!!';
+    } else if (valorIMC < 30) {
+      classificacao = 'levemente acima do peso.';
+    } else if (valorIMC < 35) {
+      classificacao = 'com obesidade grau I.';
+    } else if (valorIMC < 40) {
+      classificacao = 'com obesidade grau II';
+    } else {
+      classificacao = 'com obesidade grau III. Cuidado!!';
+    }
 
-  if (!peso) {
-    setResultado('Peso inválido', false);
-    return;
-  }
-  if (!altura) {
-    setResultado('Altura inválida', false);
-    return;
-  }
-  const imc = getImc(peso, altura);
-  const nivelImc = getNivelImc(imc);
-
-  const msg = `Seu IMC é ${imc} (${nivelImc})`;
-
-  setResultado(msg, true);
-  // Continua o código..
-});
-function getNivelImc(imc) {
-  const nivel = [
-    'Abaixo do peso',
-    'Peso normal',
-    'Sobrepeso',
-    'Obesidade grau 1',
-    'Obesidade grau 2',
-    'Obesidade grau 3',
-  ];
-
-  if (imc >= 39.9) return nivel[5]; // return vai parar a execução da função, portanto nao precisa do if else.
-
-  if (imc >= 34.9) return nivel[4];
-
-  if (imc >= 29.9) return nivel[3];
-
-  if (imc >= 24.9) return nivel[2];
-
-  if (imc >= 18.5) return nivel[1];
-
-  if (imc < 18.5) return nivel[0];
-}
-
-function getImc(peso, altura) {
-  const imc = peso / altura ** 2;
-  return imc.toFixed(2);
-}
-
-function criaP() {
-  const p = document.createElement('p'); //criando um elemento no html
-  return p;
-}
-
-function setResultado(msg, isValid) {
-  const resultado = document.querySelector('#resultado');
-  resultado.innerHTML = ''; //limpar as informações
-  // p.classList.add('paragrafo-resultado'); // adiciona uma classe na
-  // lista de classes dessa tag
-  const p = criaP(); // chamando a funçao criaP
-
-  if (isValid) {
-    p.classList.add('paragrafo-resultado');
+    resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
   } else {
-    p.classList.add('paragrafo-resultado');
+    resultado.textContent = 'Preencha todos os campos!!!';
   }
-  p.innerHTML = msg; // modificando o valor da tag original
-  resultado.appendChild(p); // inserindo essa tag p dentro da div resultado
-}
+};
 
-// function get() {
-//   const form = document.querySelector('#form');
-//   const resultado = document.querySelector('.resultado');
-//   const info = [];
-//   const event = (e) => {
-//     e.preventDefault();
-//     const peso = document.querySelector('#peso');
-//     const altura = document.querySelector('#altura');
-//     info.push({
-//       peso: peso.value,
-//       altura: altura.value,
-//     });
-//     resultado.innerHTML += `<p>${peso.value} ${altura.value}</p>`;
-//   };
-//   form.addEventListener('submit', event);
-// }
-// get();
+calcular.addEventListener('click', imc);
